482675e42fe54ca2aac5f47881fa8a086b13d3be
Jérémy TO <jeremy_to@outlook.fr>
2025-01-29 21:30:26 +0100

    change

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,7 +1,7 @@
 export const config = {
 	API_BASE_URL:
 		import.meta.env.VITE_API_BASE_URL ||
-		'https://backend-defi.onrender.com/api',
+		'https://defi-security-monitor-c64c8cf280f0.herokuapp.com/api',
 	ENV: import.meta.env.MODE,
 	IS_PRODUCTION: import.meta.env.PROD,
 };
